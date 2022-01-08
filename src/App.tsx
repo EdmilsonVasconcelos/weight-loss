d742e128da0ed8faab4dac4eaf79acc16504a710
EdmilsonVasconcelos <edmilson.vasconcelos89@gmail.com>
2022-01-08 19:43:49 -0300

    develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Login from "./pages/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Eu estou na main</h1>
-    </div>
-  );
+  return <Login />;
 }
 
 export default App;
