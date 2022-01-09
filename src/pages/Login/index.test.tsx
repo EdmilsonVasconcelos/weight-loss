347185695bdc8339965882c2e00046296ca26724
EdmilsonVasconcelos <edmilson.vasconcelos89@gmail.com>
2022-01-08 21:21:18 -0300

    teste husky

--- src/pages/Login/index.test.tsx
+++ src/pages/Login/index.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Login from '.';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Login />);
 
   const linkElement = screen.getByText(/Entrar/i);
   expect(linkElement).toBeInTheDocument();
