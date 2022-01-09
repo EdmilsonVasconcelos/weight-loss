530c15e9ea42401833c38ec2273de67481073a22
EdmilsonVasconcelos <edmilson.vasconcelos89@gmail.com>
2022-01-08 21:07:54 -0300

    teste husky

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,6 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
 
-  const linkElement = screen.getByText(/Entrarasas/i);
+  const linkElement = screen.getByText(/Entrar/i);
   expect(linkElement).toBeInTheDocument();
 });
