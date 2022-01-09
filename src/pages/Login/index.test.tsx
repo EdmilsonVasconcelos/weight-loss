6e11bc610f4671214834e47564f040ce79f565d6
EdmilsonVasconcelos <edmilson.vasconcelos89@gmail.com>
2022-01-08 21:24:12 -0300

    teste husky

--- src/pages/Login/index.test.tsx
+++
@@ -1,9 +0,0 @@
-// import { render, screen } from '@testing-library/react';
-// import Login from '.';
-
-// test('renders learn react link', () => {
-//   render(<Login />);
-
-//   const linkElement = screen.getByText(/Entrar/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
